e0ea0c95347e51c24bf613ef3c1205784e0493f3
aymene01 <aymene01@hotmail.fr>
2024-01-08 13:59:22 +0100

    add redis

--- application/app.go
+++ application/app.go
@@ -4,15 +4,19 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 type App struct {
 	router http.Handler
+	rdb    *redis.Client
 }
 
 func New() *App {
 	app := &App{
 		router: loadRoutes(),
+		rdb:    redis.NewClient(&redis.Options{}),
 	}
 	return app
 }
@@ -23,7 +27,14 @@ func (a *App) Start(cxt context.Context) error {
 		Handler: a.router,
 	}
 
-	err := server.ListenAndServe()
+	err := a.rdb.Ping(cxt).Err()
+	if err != nil {
+		return fmt.Errorf("Fail to connect db %w", err)
+	}
+
+	fmt.Println("Starting server ...")
+
+	err = server.ListenAndServe()
 	if err != nil {
 		return fmt.Errorf("Failed to run the server: %w", err)
 	}
